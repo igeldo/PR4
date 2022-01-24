7973d3331e849638fc69948eec8bc77eeb8d4f22
fhdo-fb10 <igel@fh-dortmund.de>
2022-01-24 18:51:56 +0100

    x q

---
+++ fh0010/src/test/java/fh0010/Fh0010ApplicationTests.java
@@ -0,0 +1,13 @@
+package fh0010;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fh0010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
