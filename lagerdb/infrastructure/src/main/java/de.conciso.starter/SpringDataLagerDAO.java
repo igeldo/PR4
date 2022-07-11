af2a36d82d55bdf057f3300cf97358155fc91d68
patfrenken <patrick.frenken003@stud.fh-dortmund.de>
2022-07-11 16:40:21 +0200

    test2

--- lagerdb/infrastructure/src/main/java/de.conciso.starter/SpringDataLagerDAO.java
+++ lagerdb/infrastructure/src/main/java/de.conciso.starter/SpringDataLagerDAO.java
@@ -3,5 +3,4 @@ package de.conciso.starter;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpringDataLagerDAO extends LagerDAO, CrudRepository<Lager, Integer> {
-    //
 }
